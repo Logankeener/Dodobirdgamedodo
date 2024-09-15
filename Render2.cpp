c26f991974ff5d8c49c8b6eb5cc7402b0cd42088
Logankeener <140459371+Logankeener@users.noreply.github.com>
2024-09-15 09:35:35 -0400

    Create Render2.cpp

---
+++ Render2.cpp
@@ -0,0 +1,8 @@
+void renderMenu() {
+    // Render menu elements
+    // For example, using ImGui or similar UI library
+}
+
+void renderHUD(Player& player) {
+    // Render health bar, score, etc.
+}
